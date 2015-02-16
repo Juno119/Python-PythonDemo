92731d1f53a8f1f5fe8cf460654022c9c459acd2
houminrui <hou.minrui@etaostars.com>
2015-02-16 17:01:27 +0800

    测试

--- test.py
+++ test.py
@@ -25,12 +25,12 @@ print (list + tinylist)
 tuple = ( 'abcd', 786 , 2.23, 'john', 70.2 )
 tinytuple = (123, 'john')
 
-print list
-print list[0]
-print list[1:3]
-print list[2:]
-print tinylist * 2
-print list + tinylist
+print (list)
+print (list[0])
+print (list[1:3])
+print (list[2:])
+print (tinylist * 2)
+print (list + tinylist)
 
 ###Python元字典
 dict = {}
